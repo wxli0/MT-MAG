12b1d4439d03bc912320d8eca477474d3df760cf
Wanxin Li <w328li@uwaterloo.ca>
2020-12-02 17:49:49 +0800

    debug

--- helpers.py
+++ helpers.py
@@ -204,7 +204,7 @@ def build_pipeline(num_features, classifier):
 
     # Classifier
     if classifier == 'linear-svm':
-        normalizers.append(('classifier',svm.LinearSVC()))
+        normalizers.append(('classifier',svm.SVC(kernel='linear')))
     if classifier == 'poly-svm':
         normalizers.append(('classifier',svm.SVC(kernel='poly', degree=2)))
     if classifier == 'rbf-svm':
