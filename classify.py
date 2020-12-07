76842d33198ce757faa97bfc4354f0e2efe72658
Wanxin Li <w328li@uwaterloo.ca>
2020-12-07 21:14:54 +0800

    debug

--- classify.py
+++ classify.py
@@ -80,7 +80,7 @@ def testing(test_data, k, pipeline, print_entries = False):
         print_misclassified_entries(cm)
 
     f_x = pipeline.decision_function(test_features)
-    df = pd.DataFrame(f_x, columns=list(set(pipeline.classes_)))
+    df = pd.DataFrame(f_x, columns=list(set(pipeline.classes_), index=False))
     print(df)
     path = 'outputs/fft-'+train_folder+'.xlsx'
     writer = pd.ExcelWriter(path, engine = 'xlsxwriter')
