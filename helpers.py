e922e3987b43b57888b23cecbf8f1e06d6901d7d
Wanxin Li <w328li@uwaterloo.ca>
2020-12-08 22:32:06 +0800

    added qsvm

--- helpers.py
+++ helpers.py
@@ -16,6 +16,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.metrics import confusion_matrix
 from sklearn.utils.multiclass import unique_labels
 import json
+from sklearn.preprocessing import PolynomialFeatures
 
 #extract classess from a list of tuples class-genome
 np.random.seed(42)
@@ -207,8 +208,9 @@ def build_pipeline(num_features, classifier):
     # Classifier
     if classifier == 'linear-svm':
         normalizers.append(('classifier',LinearSVC(random_state=0, tol=1e-7, multi_class='ovr', dual = False, max_iter=1000000)))
-    if classifier == 'poly-svm':
-        normalizers.append(('classifier',svm.SVC(kernel='poly', degree=2)))
+    if classifier == 'quadratic-svm':
+        normalizers.append(('poly_features', PolynomialFeatures(degree=3)))
+        normalizers.append(('classifier',LinearSVC(random_state=0, tol=1e-7, multi_class='ovr', dual = False, max_iter=1000000)))
     if classifier == 'rbf-svm':
         normalizers.append(('classifier',svm.SVC(kernel='rbf')))
     if classifier == 'lda':
