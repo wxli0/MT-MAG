09372b4b7246e31d2763ddad9a025507e9ad8b38
Wanxin Li <w328li@uwaterloo.ca>
2020-12-08 23:48:40 +0800

    added classify-linear-svm-ovo.py

--- helpers.py
+++ helpers.py
@@ -17,6 +17,7 @@ from sklearn.metrics import confusion_matrix
 from sklearn.utils.multiclass import unique_labels
 import json
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn.multiclass import OneVsOneClassifier
 
 #extract classess from a list of tuples class-genome
 np.random.seed(42)
@@ -208,6 +209,8 @@ def build_pipeline(num_features, classifier):
     # Classifier
     if classifier == 'linear-svm':
         normalizers.append(('classifier',LinearSVC(random_state=0, tol=1e-7, multi_class='ovr', dual = False, max_iter=1000000)))
+    if classifier == 'linear-svm-ovo':
+        normalizers.append(('classifier', OneVsOneClassifier(LinearSVC(random_state=0, tol=1e-7, multi_class='ovr', dual = False, max_iter=1000000))))
     if classifier == 'quadratic-svm':
         normalizers.append(('poly_features', PolynomialFeatures(degree=2)))
         normalizers.append(('classifier',LinearSVC(random_state=0, tol=1e-7, multi_class='ovr', dual = False, max_iter=10000)))
