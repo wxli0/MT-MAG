9d573beed05d4219d5d5bfdc25cd791b5cbc7830
Wanxin Li <w328li@uwaterloo.ca>
2020-12-08 15:19:09 +0800

    changed folder structure

--- classify-linear-svm.py
+++ classify-linear-svm.py
@@ -16,79 +16,17 @@ from sklearn.metrics import confusion_matrix
 from helpers import getStats, plotDict, kmer_count, build_pipeline, plot_confusion_matrix, get_misclassified_entries, print_misclassified_entries
 import sys
 from helpers import entries_count
+from classifyhelpers import testing, training, read_pfiles
 
 #e.g. python3 classify.py o__Bacteroidales_exclude_g__C941 g__C941_wrapper
-
-np.set_printoptions(suppress=True)
-train_folder = sys.argv[1]
-test_folder = sys.argv[2]
-
-dest_folder = "p_files/"
-
-train_filename = dest_folder+train_folder+'.p'
-test_filename = dest_folder+test_folder+'.p'
-
-with open(train_filename, 'rb') as f:
-    train = pickle.load(f)
-
-with open(test_filename, 'rb') as f:
-    test = pickle.load(f)
-
 
 def f_to_c(theta):
     return -2*max(0, 1+theta)/(-2*max(0, 1+theta)-2*max(0, 1-theta))
 
-def training(train_data, k, classifier):
-    train_features = []
-    train_labels = []
-
-    for i in range(len(train_data)):
-        train_features.append(kmer_count(train_data[i][1], k))
-        train_labels.append(train_data[i][0])
-
-    x = np.asarray(train_features).astype('float32')
-    # x = normalize(x, norm='l2', axis=1)
-    y = np.asarray(train_labels)
-
-    # features = normalize(x, norm='l2', axis=1)
-    features = np.abs(np.fft.fft(x))
-    subtypes = np.asarray(train_labels)
-
-    # Run the classification Pipeline for this subset.
-    pipeline = build_pipeline(4 ** k, classifier)
-    pipeline.fit(features, subtypes)
-    if classifier == 'linear-svm':
-        pipeline.prefix = 'LS'
-    elif classifier == 'lda':
-        pipeline.prefix = 'LDA'
-
-    return pipeline
 
 
-def testing(test_data, k, pipeline, print_entries = False):
-    test_features = []
-    test_labels = []
-    test_ids = []
-
-    for i in range(len(test_data)):
-        test_features.append(kmer_count(test_data[i][1], k))
-        test_labels.append(test_data[i][0])
-        test_ids.append(test_data[i][2])
-
-    x = np.asarray(test_features).astype('float32')
-    y = np.asarray(test_labels)
-
-    # test_features = normalize(x, norm='l2', axis=1)
-    test_features = np.abs(np.fft.fft(x))
-
-    y_pred = pipeline.predict(test_features)
-    print("y is:", y)
-    print("y_pred is:", y_pred)
-    cm = confusion_matrix(y, y_pred)
-    if print_entries:
-        print(cm)
-        print("printing misclassified entries")
-        print_misclassified_entries(cm)
+def testing_lsvm(test_data, k, pipeline, print_entries = False):
+    test_features, y_pred, test_ids, y = testing(test_data, k, pipeline)
 
     f_x = pipeline.decision_function(test_features)
     f_to_c_vec = np.vectorize(f_to_c)
@@ -120,14 +58,20 @@ def testing(test_data, k, pipeline, print_entries = False):
 
 
 print('************ classify new sequences ************************')
+np.set_printoptions(suppress=True)
+train_folder = sys.argv[1]
+test_folder = sys.argv[2]
+
+dest_folder = "p_files/"
+
+train_filename = dest_folder+train_folder+'.p'
+test_filename = dest_folder+test_folder+'.p'
+
+train, test = read_pfiles(train_filename, test_filename)
 
 k = 7
-# classifiers = ['linear-svm', 'poly-svm', 'rbf-svm', 'lda', 'KNN']
-classifiers = ['linear-svm']
-
-
-for classifier in classifiers:
-    pipeline = training(train, k, classifier)
-    acc = testing(test, k, pipeline)
-    print(classifier+":", acc)
+classifiers = 'linear-svm'
+pipeline = training(train, k, classifier)
+acc = testing_lsvm(test, k, pipeline)
+print(classifier+":", acc)
 
---
+++ classifyhelpers.py
@@ -0,0 +1,58 @@
+# Dependencies
+import numpy as np
+from helpers import getStats, plotDict, kmer_count, build_pipeline, plot_confusion_matrix, get_misclassified_entries, print_misclassified_entries
+import sys
+from helpers import entries_count
+
+def testing(test_data, k, pipeline):
+    test_features = []
+    test_labels = []
+    test_ids = []
+
+    for i in range(len(test_data)):
+        test_features.append(kmer_count(test_data[i][1], k))
+        test_labels.append(test_data[i][0])
+        test_ids.append(test_data[i][2])
+
+    x = np.asarray(test_features).astype('float32')
+    y = np.asarray(test_labels)
+
+    # test_features = normalize(x, norm='l2', axis=1)
+    test_features = np.abs(np.fft.fft(x))
+
+    y_pred = pipeline.predict(test_features)
+    return test_features, y_pred, test_ids, y
+
+
+def training(train_data, k, classifier):
+    train_features = []
+    train_labels = []
+
+    for i in range(len(train_data)):
+        train_features.append(kmer_count(train_data[i][1], k))
+        train_labels.append(train_data[i][0])
+
+    x = np.asarray(train_features).astype('float32')
+    # x = normalize(x, norm='l2', axis=1)
+    y = np.asarray(train_labels)
+
+    # features = normalize(x, norm='l2', axis=1)
+    features = np.abs(np.fft.fft(x))
+    subtypes = np.asarray(train_labels)
+
+    # Run the classification Pipeline for this subset.
+    pipeline = build_pipeline(4 ** k, classifier)
+    pipeline.fit(features, subtypes)
+    if classifier == 'linear-svm':
+        pipeline.prefix = 'LS'
+    elif classifier == 'lda':
+        pipeline.prefix = 'LDA'
+
+    return pipeline
+
+def read_pfiles(train_filename, test_filename):
+    with open(train_filename, 'rb') as f:
+        train = pickle.load(f)
+    with open(test_filename, 'rb') as f:
+        test = pickle.load(f)
+    return train, test
