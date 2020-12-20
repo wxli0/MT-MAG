465cab21e3cec31aad24e17d34b2ee99621347da
Wanxin Li <w328li@uwaterloo.ca>
2020-12-20 22:25:02 +0800

    debug

--- classify-linear-svm-ovr.py
+++ classify-linear-svm-ovr.py
@@ -61,7 +61,7 @@ def testing_lsvm(test_data, k, pipeline, print_entries = False):
     df_post = pd.DataFrame(f_post, columns=labels)
     df_post.index = test_ids
     df_post['prediction'] = y_pred
-    df_post1 = pd.DataFrame(f_post, columns=labels)
+    df_post1 = pd.DataFrame(f_post1, columns=labels)
     df_post1.index = test_ids
     df_post1['prediction'] = y_pred
     df['prediction'] = y_pred
