777233cf74e7e2f591f1a867b5bd74f45e441429
Wanxin Li <w328li@uwaterloo.ca>
2021-01-18 20:20:10 +0800

    updated run_preprocessing.py

--- run_preprocessing.py
+++ run_preprocessing.py
@@ -1,10 +1,7 @@
 import os
 
-files = ["f__F082_eval_wrapper", "f__Lachnospiracea_wrapper", "f__Marinifilaceae_eval_wrapper",
-    "f__Muribaculaceae_eval_wrapper", "f__Paludibacteraceae_eval_wrapper", "f__Porphyromonadaceae_eval_wrapper",
-    "f__Prolixibacteraceae_eval_wrapper", "f__PUMT01_eval_wrapper", "f__Rikenellaceae_eval_wrapper",
-    "f__Tannerellaceae_eval_wrapper", "f__UBA7960_eval_wrapper", "f__UBA932_eval_wrapper",
-    "f__VadinHA17_eval_wrapper", "f__WCHB1-69_eval_wrapper"]
+files = ["f__Acutalibacteraceae_eval_wrapper",  "f__CAG-272_eval_wrapper", "f__Oscillospiraceae_eval_wrapper",
+"f__Butyricicoccaceae_eval_wrapper",   "f__CAG-382_eval_wrapper",  "f__Ruminococcaceae_eval_wrapper"]
 
 for file in files:
     os.system("python3 preprocessing.py "+file)
