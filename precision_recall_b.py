cfe7c092a1ff6cba9620053c076226e05e915808
Wanxin Li <w328li@uwaterloo.ca>
2021-01-19 22:48:54 +0800

    updated outputs with 0.75

--- precision_recall_b.py
+++ precision_recall_b.py
@@ -11,6 +11,7 @@ file_name = sys.argv[1]
 xls = pd.ExcelFile(file_name)
 precision = []
 recall = []
+weighted = []
 
 for alpha in alphas:
     reads = 0
@@ -25,19 +26,25 @@ for alpha in alphas:
             reads += len(predicted)
             unassigned += predicted.count('reject')
             correct += predicted.count(sheet[:-2])
-    p = correct/(reads-unassigned)
+    p = 1
+    if (reads-unassigned) != 0:
+        p = correct/(reads-unassigned)
     r = correct/reads
+    w = 0.5*p+0.5*r
     precision.append(p)
     recall.append(r)
-    print("alpha =", alpha, "precision:", p, "recall:", r)
+    weighted.append(w)
+    print("alpha =", alpha, "precision:", p, "recall:", r, "weighted:", w)
 
 
 
 plt.xticks(alphas[::5],  rotation='vertical')
 plt.xlabel("threshould")
 plt.ylabel("precision/recall")
-plt.plot(alphas, precision, 'o-', label="Precision", markersize=5)
-plt.plot(alphas, recall, 'o-', label="Recall", markersize=5)
+plt.plot(alphas, precision, 'o-', label="Precision", markersize=2)
+plt.plot(alphas, recall, 'o-', label="Recall", markersize=2)
+plt.plot(alphas, weighted, 'o-', label="Weighted", markersize=2)
+plt.axhline(y=0.75, color='r', linestyle='-')
 plt.legend()
 plt.savefig(file_name[:-5]+'-pr.png')
 plt.show()
