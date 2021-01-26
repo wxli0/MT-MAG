da1af1d4586df2864194611f67c8426a6e46ef88
Wanxin Li <w328li@uwaterloo.ca>
2021-01-26 12:27:27 +0800

    debug

--- simple_notebook_rejection.py
+++ simple_notebook_rejection.py
@@ -137,12 +137,13 @@ def train(model, optimizer, loss_func, x, y, iterations):
         # clip gradients
         clipping_value = 1 
         torch.nn.utils.clip_grad_norm(model.parameters(), clipping_value)
+        print("***** iteration:", iteration, "******")
         print("print gradients:")
         for p in model.parameters():
             if p.grad is not None:
                 print(p.grad.data)
         optimizer.step()
-        print("iteration:", i, "loss:", loss.item())
+        print("loss:", loss.item())
 
 """---
 
