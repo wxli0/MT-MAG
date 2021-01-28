9b240cf84067a9e99a9b896ab664ba9ca573aa0a
Wanxin Li <w328li@uwaterloo.ca>
2021-01-28 15:08:07 +0800

    updated run_preprocessing.py

--- run_preprocessing.py
+++ run_preprocessing.py
@@ -2,8 +2,8 @@ import os
 import sys
 
 dir = sys.argv[1]
-files = os.listdir(dir)
+files = os.listdir(os.path.join("data", dir))
 
 for file in files:
-    os.system("python3 preprocessing.py "+os.path.join(dir,file))
+    os.system("python3 preprocessing.py "+os.path.join("data", dir,file))
     print(file + "done")
