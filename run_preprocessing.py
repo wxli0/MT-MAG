266eaa5b9596e93a6b4352f1b54feacc3124316e
Wanxin Li <w328li@uwaterloo.ca>
2021-01-28 02:06:30 -0500

    aupdated run_preprocessing.py

--- run_preprocessing.py
+++ run_preprocessing.py
@@ -2,9 +2,8 @@ import os
 import sys
 
 dir = sys.argv[1]
-files = os.listdir(os.path.join('/home/w328li/MLDSP/samples', dir))
+files = os.listdir(dir)
 
 for file in files:
-    file = file+"_wrapper"
-    os.system("python3 preprocessing.py "+file)
+    os.system("python3 preprocessing.py "+os.path.join(dir,file))
     print(file + "done")
