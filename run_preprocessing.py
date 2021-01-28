5cf239e9c94034def291b8d027aba906e9463648
Wanxin Li <w328li@uwaterloo.ca>
2021-01-28 15:10:32 +0800

    debug

--- run_preprocessing.py
+++ run_preprocessing.py
@@ -5,6 +5,6 @@ dir = sys.argv[1]
 files = os.listdir(os.path.join("data", dir))
 
 for file in files:
-    if not file.endswith('.json')
+    if not file.endswith('.json'):
         os.system("python3 preprocessing.py "+os.path.join(dir,file))
         print(file + "done")
