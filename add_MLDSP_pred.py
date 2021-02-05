3e3a0a787e1d639a58e4ef84d292e380e035d593
Wanxin Li <w328li@uwaterloo.ca>
2021-02-05 21:35:04 +0800

    udpated clasify_meta MLDSP-prediction.csv

--- add_MLDSP_pred.py
+++ add_MLDSP_pred.py
@@ -1,9 +1,12 @@
 import sys
 import pandas as pd
+import math
+import openpyxl
 
-file = sys.argv[1]
-file_short = file.split('/')[-1]
-sheet = file_short[:-5]+"_pred_t"
+file_path = sys.argv[1]
+file_short = file_path.split('/')[-1]
+sheet = file_short[:-5]+"_pred-t-p"
+MLDSP_pred_path = "outputs/MLDSP-prediction.csv"
 
 taxon = ""
 if sheet.startswith('d'):
@@ -16,4 +19,16 @@ elif sheet.startswith('o'):
     taxon = "family"
 
 df = pd.read_excel(file_path, index_col=0, header=0, sheet_name=sheet)
-MLDSP_df =  pd.read_excel("outputs/MLDSP-prediction", index_col=0, header=0, sheet_name="Sheet1")
+MLDSP_df =  pd.read_csv(MLDSP_pred_path, index_col=0, header=0, dtype = str)
+# print(MLDSP_df)
+# print(math.isnan(MLDSP_df.loc['RUG518']['family']))
+for index, row in df.iterrows():
+    if not math.isnan(MLDSP_df.loc[index][taxon]):
+        print("index is:", index)
+        print("taxon is:", taxon)
+        raise Exception()
+    MLDSP_df.at[index, taxon] = row['rejection-f']
+
+print(MLDSP_df)
+
+MLDSP_df.to_csv(MLDSP_pred_path, index=True, header=True)
