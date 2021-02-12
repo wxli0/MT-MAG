efb1eec40d713fc0ba794bb1e8e6b53d0d58745a
Wanxin Li <w328li@uwaterloo.ca>
2021-02-12 15:48:28 +0800

    debug

--- rejection-ova.py
+++ rejection-ova.py
@@ -233,10 +233,12 @@ for i in range(len(tests)):
     out_test = model_ova(x_tensor_test)
     df = pd.DataFrame(out_test)
     df.columns = y_unique
-    df = df.set_index(test_ids)
-    df['rejection'] = rejected
+    df['ID'] = test_ids
+    df = df.set_index('ID')
     print("df is:", df)
     rejected = conf_reject(-links[loss_name_ova](rej_cost))(out_test)
+    df['rejection'] = rejected
+    df.to_csv('outputs/ova/'+train_folder+'-'+test_folders.split('/')[-1]+'.csv')
     # print("rejected is:", rejected)
     # result = torch.zeros_like(y_tensor_test)
     # result[rejected] = -1
