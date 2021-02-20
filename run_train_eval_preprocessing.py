91e5b72a523d35a7dbeba98a2b850e6ba12bab7c
Wanxin Li <w328li@uwaterloo.ca>
2021-02-20 20:35:08 +0800

    added run_train_eval_preprocessing.py

---
+++ run_train_eval_preprocessing.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+# e.g. python3 run_train_eval_preprocessing,py everything
+dir = sys.argv[1]
+files = os.listdir(os.path.join("data", dir))
+
+os.sys("python3 preprocessing.py "+os.path.join(dir))
+os.sys("python3 preprocessing.py "+os.path.join(dir+"_train"))
+os.sys("python3 run_preprocessing.py "+os.path.join(dir))
