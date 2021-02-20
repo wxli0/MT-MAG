6b77a557d76e0e5c8f22188dd171cbe79a63f611
Wanxin Li <w328li@uwaterloo.ca>
2021-02-20 20:36:21 +0800

    renamed

--- run_train_eval_proprocessing.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-dir = sys.argv[1]
-files = os.listdir(os.path.join("data", dir))
-
-os.sys("python3 preprocessing.py "+os.path.join(dir))
-os.sys("python3 preprocessing.py "+os.path.join(dir+"_train"))
-os.sys("python3 run_preprocessing.py "+os.path.join(dir))
