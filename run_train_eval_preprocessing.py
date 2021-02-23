bea05579d4a05209ee256cb36935d28636b65150
Wanxin Li <w328li@uwaterloo.ca>
2021-02-23 20:02:57 +0800

    debug

--- run_train_eval_preprocessing.py
+++ run_train_eval_preprocessing.py
@@ -3,8 +3,7 @@ import sys
 
 # e.g. python3 run_train_eval_preprocessing.py everything
 dir = sys.argv[1]
-files = os.listdir(os.path.join("data", dir))
 
-os.sys("python3 preprocessing.py "+os.path.join(dir))
-os.sys("python3 preprocessing.py "+os.path.join(dir+"_train"))
-os.sys("python3 run_preprocessing.py "+os.path.join(dir))
+os.sys("python3 preprocessing.py "+dir)
+os.sys("python3 preprocessing.py "+dir+"_train")
+os.sys("python3 run_preprocessing.py "+dir+"_eval")
