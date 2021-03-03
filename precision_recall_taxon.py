a1e8e3dfe3f3a6894b36b1dae145b3f120f1611e
Wanxin Li <w328li@uwaterloo.ca>
2021-03-03 22:29:09 +0800

    updated outputs

--- precision_recall_taxon.py
+++ precision_recall_taxon.py
@@ -5,6 +5,9 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import os
+import shutil
+import json 
+import platform
 
 # python3 precision_recall_b.py outputs/fft-o__Oscillospirales.xlsx
 
@@ -18,6 +21,8 @@ precision = []
 recall = []
 weighted = []
 
+thres_alpha = 0
+done = 0
 for alpha in alphas:
     reads = 0
     correct = 0
@@ -42,6 +47,9 @@ for alpha in alphas:
     p_weight = 0.5
     w = p_weight*p+(1-p_weight)*r
     precision.append(p)
+    if not done and p > 0.95:
+        thres_alpha = alpha
+        done = True
     recall.append(r)
     weighted.append(w)
     print("alpha =", alpha, "precision:", p, "recall:", r, "weighted:", w)
@@ -59,5 +67,24 @@ plt.legend()
 
 plt.savefig(file_name[:-5]+'-'+taxon+'-pr.png')
 # plt.show()
+
+BK_path = "/Users/wanxinli/Desktop/project/BlindKameris-new/rejection_threshold/"
+if platform.platform()[:5] == 'Linux':
+    BK_path = "/home/w328li/BlindKameris-new/outputs/rejection_threshold/"
+
+rej_path = BK_path+file_name.split('/')[-1][:-11]+'.json'
+
+if not os.path.isfile(rej_path):
+    rej_dict = {taxon: thres_alpha}
+    with open(rej_path, 'w') as f:
+        json.dump(rej_dict, f)
+else:
+    with open(rej_path) as json_file:
+        rej_dict = json.load(json_file)
+        rej_dict[taxon] = thres_alpha
+
+    with open(rej_path, 'w') as json_file:
+        json.dump(rej_dict, json_file)
+
 
 
