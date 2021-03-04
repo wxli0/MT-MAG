34f0af1719441c26a5415d599c4a9e6320b03a28
Wanxin Li <w328li@uwaterloo.ca>
2021-03-04 11:26:32 +0800

    udpated outputs

--- add_single_child.py
+++ add_single_child.py
@@ -19,9 +19,9 @@ MLDSP_df =  pd.read_csv(MLDSP_pred_path, index_col=0, header=0, dtype = str)
 # print(math.isnan(MLDSP_df.loc['RUG518']['family']))
 for index, row in MLDSP_df.iterrows():
     if row[taxon] in rep_dict:
-        print("index is:", index)
+        print("index is:", index, "label is:",row[taxon])
         next_taxon= taxons[taxons.index(taxon)+1]
-        MLDSP_df.at[index, next_taxon] = row[rep_dict[row[taxon]]]
+        MLDSP_df.at[index, next_taxon] = rep_dict[row[taxon]]
 
 print(MLDSP_df)
 
