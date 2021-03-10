70d69ffec7ab604ec7777b41ca4f81e5e9f12d21
Wanxin Li <w328li@uwaterloo.ca>
2021-03-10 12:21:44 +0800

    debug

--- run_precision_recall_b_taxon.py
+++ run_precision_recall_b_taxon.py
@@ -17,6 +17,7 @@ xls = pd.ExcelFile(file_name)
 for sheet in xls.sheet_names:
     if sheet.endswith('-p'):
         print(BK_path+"precision_recall_taxon.py ")
+        print(BK_path+file_name)
         print("before python3 "+BK_path+"precision_recall_taxon.py " + BK_path+file_name+ " " +  '"'+sheet[:-4] +'"'+ " > " + BK_path+file_name[:-5]+'-'+sheet[:-4]+'-pr-log.txt')
         # os.system("python3 precision_recall_taxon.py " + file_name+ " " +  sheet[:-4])
 
