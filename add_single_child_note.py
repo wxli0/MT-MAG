ad1a424f4ed8868946fba0f92d6d0ef2e47911aa
Wanxin Li <w328li@uwaterloo.ca>
2021-03-11 18:27:06 +0800

    updated full-path

--- add_single_child_note.py
+++ add_single_child_note.py
@@ -7,9 +7,9 @@ rank = sys.argv[1]
 next_rank = sys.argv[2]
 MLDSP_pred_path = "/Users/wanxinli/Desktop/project/BlindKameris-new/outputs/MLDSP-prediction-full-path.csv"
 MLDSP_df =  pd.read_csv(MLDSP_pred_path, index_col=0, header=0, dtype = str)
-print(MLDSP_df)
+# print(MLDSP_df)
 
-gtdb_tk_taxon_path = "/Users/wanxinli/Desktop/project/MLDSP-desktop/outputs/gtdbtk/gtdb-taxon.csv"
+gtdb_tk_taxon_path = "/Users/wanxinli/Desktop/project/MLDSP-desktop/data/DS_10283_3009/taxon.csv"
 gtdb_tk_df = pd.read_csv(gtdb_tk_taxon_path, header=0, dtype = str)
 gtdb_tk_df = gtdb_tk_df[[rank, next_rank]]
 # gtdb_tk_df = gtdb_tk_df.set_index(['genus'])
@@ -25,15 +25,15 @@ for index, row in MLDSP_df.iterrows():
 
     
     children = gtdb_tk_df.loc[parent][next_rank]
+    # print(children)
     if next_rank[:1]+"__" in children:
         children.remove(next_rank[:1]+"__")
-    print(children)
     if len(children) == 1:
-        print("enter this clause")
         only_child = children[0]
+        print("enter this clause:", parent, children)
         MLDSP_df.at[index, next_rank] = "# only one child "+ only_child
 
-# print(MLDSP_df)
+print(MLDSP_df)
 
 
 MLDSP_df.to_csv(MLDSP_pred_path, index=True, header=True)
