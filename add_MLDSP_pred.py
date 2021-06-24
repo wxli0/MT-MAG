490bf4f9cad80ad665192d901965ae0d1cd97674
Wanxin Li <w328li@uwaterloo.ca>
2021-06-24 11:33:59 +0800

    removed prints

--- add_MLDSP_pred.py
+++ add_MLDSP_pred.py
@@ -31,13 +31,13 @@ MLDSP_df =  pd.read_csv(MLDSP_pred_path, index_col=0, header=0, dtype = str)
 # print(MLDSP_df)
 # print(math.isnan(MLDSP_df.loc['RUG518']['family']))
 for index, row in df.iterrows():
-    print("index is:", index)
-    print("taxon is:", taxon)
+    # print("index is:", index)
+    # print("taxon is:", taxon)
     # if not math.isnan(MLDSP_df.loc[index[:-3]][taxon]):
     #     print("success")
     #     raise Exception()
     MLDSP_df.at[index[:-3], taxon] = row['rejection-f']
 
-print(MLDSP_df)
+# print(MLDSP_df)
 
 MLDSP_df.to_csv(MLDSP_pred_path, index=True, header=True)
