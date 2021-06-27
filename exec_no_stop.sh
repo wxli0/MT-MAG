54fd1a97574d4bb2d17f9bcac0613f30b16dd17c
Wanxin Li <w328li@uwaterloo.ca>
2021-06-27 15:04:28 +0800

    updated exec_no_stop.sh

--- exec_no_stop.sh
+++ exec_no_stop.sh
@@ -3,7 +3,9 @@ while true
 do  
     echo "==== begin group_pred ===="
     python3 outputs-HGR-r202/group_pred_HGR.py family
+    python3 outputs-HGR-r202/group_pred_HGR.py genus
     echo "==== begin checking_missing_exec ====" 
     python3 checking_missing_exec.py True
-    sleep 10  
+    echo "==== begin sleep ===="
+    sleep 1600  
 done
