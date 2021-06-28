53550b2bee57a1c8e7ef8423307b1fe668a3f5c7
Wanxin Li <w328li@uwaterloo.ca>
2021-06-27 23:19:48 -0400

    updated outputs

---
+++ outputs-HGR-r202/group_pred_HGR.py
@@ -0,0 +1,32 @@
+import sys
+import pandas as pd 
+import json
+import os
+from shutil import copyfile
+import platform
+import shutil
+import numpy as np
+
+# e.g. python3 group_pred.py order
+taxon = sys.argv[1]
+
+BK_path = "/Users/wanxinli/Desktop/project/BlindKameris-new/"
+base_path = "/mnt/sda/DeepMicrobes-data/" 
+if platform.platform()[:5] == 'Linux':
+    BK_path = "/home/w328li/BlindKameris-new/"
+
+MLDSP_pred_path = BK_path+"outputs-HGR-r202/HGR-prediction-full-path.csv"
+MLDSP_df =  pd.read_csv(MLDSP_pred_path, index_col=0, header=0, dtype = str)
+print(MLDSP_df)
+
+for index, row in  MLDSP_df.iterrows():
+    label = row[taxon]
+    if index.endswith('_1') or index.endswith('_2'):
+        continue
+    # print("label is:", label)
+    if not str(label) == 'nan':
+        # print("enter")
+        if not os.path.isdir(base_path+"labeled_genome-r202/hgr_mags/"+label):
+            os.mkdir(base_path+"labeled_genome-r202/hgr_mags/"+label)
+        copyfile(base_path+"mag_reads_250bp_1w_200000/"+index+".fa", base_path+"labeled_genome-r202/hgr_mags/"+label+"/"+index+".fa")
+        
