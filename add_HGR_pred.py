e7551dc0f9349b85b2306036e3a947d57de89410
Wanxin Li <w328li@uwaterloo.ca>
2021-06-30 18:21:16 +0800

    added lock

--- add_HGR_pred.py
+++ add_HGR_pred.py
@@ -4,37 +4,36 @@ import math
 import openpyxl
 
 # e.g. 
-file_path = sys.argv[1]
-file_short = file_path.split('/')[-1]
-ver = file_path.split('/')[0].split('-')[-1]
-sheet = file_short[:-5]+"_pred-t-p"
-if len(sys.argv) == 3:
-    sheet = sys.argv[2]
-MLDSP_pred_path = "outputs-HGR-"+ver+"/HGR-prediction-full-path.csv"
+from filelock import FileLock
 
-taxon = ""
-if sheet.startswith('d') or sheet.startswith('e'):
-    taxon = "phylum"
-elif sheet.startswith('p'):
-    taxon = "class"
-elif sheet.startswith('c'):
-    taxon = "order"
-elif sheet.startswith('o'):
-    taxon = "family"
-elif sheet.startswith('f'):
-    taxon = "genus"
-elif sheet.startswith('g'):
-    taxon = "species"
+with FileLock("myfile.txt.lock"):
+    print("Lock acquired.")
 
-df = pd.read_excel(file_path, index_col=0, header=0, sheet_name=sheet)
-MLDSP_df =  pd.read_csv(MLDSP_pred_path, index_col=0, header=0, dtype = str)
-# print(MLDSP_df)
-# print(math.isnan(MLDSP_df.loc['RUG518']['family']))
-for index, row in df.iterrows():
-    MLDSP_df.at[index[:-3], taxon] = row['prediction'] # used to be 'rejection-f'
+    file_path = sys.argv[1]
+    file_short = file_path.split('/')[-1]
+    ver = file_path.split('/')[0].split('-')[-1]
+    sheet = file_short[:-5]+"_pred-t-p"
+    if len(sys.argv) == 3:
+        sheet = sys.argv[2]
+    MLDSP_pred_path = "outputs-HGR-"+ver+"/HGR-prediction-full-path.csv"
 
-# print(MLDSP_df)
-# print(MLDSP_df.shape)
-# print(df.shape)
+    taxon = ""
+    if sheet.startswith('d') or sheet.startswith('e'):
+        taxon = "phylum"
+    elif sheet.startswith('p'):
+        taxon = "class"
+    elif sheet.startswith('c'):
+        taxon = "order"
+    elif sheet.startswith('o'):
+        taxon = "family"
+    elif sheet.startswith('f'):
+        taxon = "genus"
+    elif sheet.startswith('g'):
+        taxon = "species"
 
-MLDSP_df.to_csv(MLDSP_pred_path, index=True, header=True)
+    df = pd.read_excel(file_path, index_col=0, header=0, sheet_name=sheet)
+    MLDSP_df =  pd.read_csv(MLDSP_pred_path, index_col=0, header=0, dtype = str)
+    for index, row in df.iterrows():
+        MLDSP_df.at[index[:-3], taxon] = row['prediction'] # used to be 'rejection-f'
+
+    MLDSP_df.to_csv(MLDSP_pred_path, index=True, header=True)
