c1ee2a2d1ac51663b9cc0194288d9cfe09736650
Wanxin Li <w328li@uwaterloo.ca>
2021-07-14 00:08:14 +0800

    updated result_stat.py

--- result_stat.py
+++ result_stat.py
@@ -2,15 +2,24 @@ import os
 import pandas as pd
 
 
-# calculates the precision, recall, incorrect rate, partial recall
-def calc_stats(path, ranks):
+# calculates the precision, recall, incorrect rate, partial recall, rejection rate at different levels
+def calc_stats(path, ranks, ignore_taxons =[]):
     df = pd.read_csv(path, header=0, index_col=0)
     partial_correct = 0
     correct = 0
     incorrect = 0
     rejected = 0
     total = 0
+    # init rej_stats
+    rej_stats = {}
+    for r in ranks:
+        rej_stats[r] = 0
+
     for index, row in df.iterrows():
+        if df.loc[index]['gtdb-tk-species'] == 's__':
+            continue
+        if df.loc[index]['genus'] in ignore_taxons:
+            continue
         for i in range(len(ranks)):
             cur_rank = ranks[i]
             cur_pred = str(df.loc[index][cur_rank])
@@ -35,43 +44,35 @@ def calc_stats(path, ranks):
                 incorrect += 1
                 total += 1
                 break
-                
-    return correct/(total-rejected), correct/total, incorrect/total, partial_correct/total
 
-
-
-# compute rejection level statistics 
-def rej_stats(path, ranks):
-    # init stats
-    stats = {}
-    for r in ranks:
-        stats[r] = 0
-
-    df = pd.read_csv(path, header=0, index_col=0)
     for index, row in df.iterrows():
         for r in ranks:
             if 'reject' in str(df.loc[index][r]):
-                stats[r] += 1
+                rej_stats[r] += 1
                 break
-    for k in stats:
-        stats[k] /= df.shape[0]
-    return stats
+            
+    for k in rej_stats:
+        rej_stats[k] /= total
+                
+    return correct/(total-rejected), correct/total, incorrect/total, partial_correct/total, rej_stats
+
+
+
 
 path1 = "/Users/wanxinli/Desktop/project.nosync/BlindKameris-new/outputs-r202-archive1/MLDSP-prediction-full-path-archive.csv"
 ranks1 = ['domain', 'phylum', 'class', 'order', 'family', 'genus', 'species']
-precision1, recall1, incorrect_rate1, partial_recall1 = calc_stats(path1, ranks1)
+precision1, recall1, incorrect_rate1, partial_recall1, rej_stats1 = calc_stats(path1, ranks1)
 print("precision is:", precision1, "recall is:", recall1, "incorrect rate is:", incorrect_rate1, "partial recall is:", partial_recall1)
-# rej_stat1= rej_stats(path1, ranks1)
-# for r in rej_stat1:
-#     print(rej_stat1[r], "rejects at ", r)
+for r in rej_stats1:
+    print(rej_stats1[r], "rejects at ", r)
 
 path2 = "/Users/wanxinli/Desktop/project.nosync/BlindKameris-new/outputs-HGR-r202-archive1/HGR-prediction-full-path.csv"
 ranks2 = ['phylum', 'class', 'order', 'family', 'genus', 'species']
-precision2, recall2, incorrect_rate2, partial_recall2 = calc_stats(path2, ranks2)
+ignore_taxons2 = ['g__Pediococcus', 'g__Ruminococcus_F', 'g__F0040']
+precision2, recall2, incorrect_rate2, partial_recall2, rej_stats2 = calc_stats(path2, ranks2)
 print("precision is:", precision2, "recall is:", recall2, "incorrect rate is:", incorrect_rate2, "partial recall is:", partial_recall2)
-# rej_stat2 = rej_stats(path2, ranks2)
-# for r in rej_stat2:
-#     print(rej_stat2[r], "rejects at ", r)
+for r in rej_stats2:
+    print(rej_stats2[r], "rejects at ", r)
 
 
 
