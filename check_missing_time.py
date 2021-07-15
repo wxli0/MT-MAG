c68d48ff6340adbbce3d7355147a35b867f074ec
Wanxin Li <w328li@uwaterloo.ca>
2021-07-15 12:56:47 +0800

    added exec_time.sh

---
+++ check_missing_time.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import os
+import sys
+import platform
+import subprocess
+
+# check missing predictions in HGR/MLDSP-prediction-full-path.csv
+
+
+def check_missing(path, time_cat):
+    df = pd.read_csv(path, index_col=0, header=0, dtype=str)
+
+    missing_ranks = []
+    for index, row in df.iterrows():
+        if str(row[time_cat]) == "nan":
+            missing_ranks.append(index)
+
+    return missing_ranks
+path1 = '/Users/wanxinli/Desktop/project.nosync/BlindKameris-new/outputs-r202/time.csv'
+time_cat1 = 'rej_time'
+missing_ranks1 = check_missing(path1, time_cat1)
+
+for rank in missing_ranks1:
+    running_proc = str(subprocess.check_output("ps aux|grep w328li|grep "+rank, shell=True))
+    proc_all =  str(subprocess.check_output("screen -ls", shell=True))
+    if running_proc.count('\\n') <= 2 and proc_all.count('\\n') <= 40:
+        os.system('screen -dm bash -c "cd ~/MLDSP; bash phase_3_4.sh '+rank + '"')
+        print('enter screen -dm bash -c "cd ~/MLDSP; bash phase_3_4.sh '+rank + '"')
+    elif proc_all.count('\\n') > 40:
+        print('too many processes running')
+    else:
+        print(rank, "in GTDB running process")
---
+++ exec_time.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+i=0  
+while true  
+do  
+    echo "iteration ${i}"
+    echo "==== git commit ===="
+    cd ~/MLDSP
+    git add .
+    git commit -m "updated outputs"
+    git push
+    cd ~/BlindKameris-new
+    git add .
+    git commit -m "updated outputs"
+    git push
+    echo "==== begin checking_missing_time ====" 
+    python3 checking_missing_time.py 
+    echo "==== begin sleep 5 minutes at $(date)===="
+    sleep 300
+    i=$((i+1))  
+done
