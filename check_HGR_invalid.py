740ba84ed3121473681c917480c9c46807f85351
Wanxin Li <w328li@uwaterloo.ca>
2021-07-16 10:30:50 +0800

    added check_HGR_invalid.py

---
+++ check_HGR_invalid.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import pandas as pd
+
+HGR_path = '~/BlindKameris-new/outputs-HGR-r202/HGR-prediction-full-path.csv'
+HGR_df =  pd.read_csv(HGR_path, index_col=0, header=0, dtype = str)
+
+data_dir = '/mnt/sda/DeepMicrobes-data/labeled_genome-r202/'
+
+single_child_genus = []
+all_species = []
+for dir in os.listdir(data_dir):
+    # single child for genus 
+    if dir.startswith('g__') and dir.endswith('_split_pruned'):
+        dir_path = data_dir + dir + '/'
+        children = os.listdir(dir_path)
+        if len(children) == 1:
+            single_child_genus.append(dir)
+        else:
+            all_species.extend(children)
+
+index_species_not_exist = []
+index_genus_single_child = []
+for index, row in HGR_df.iterrows():
+    # ground-truth label does not exist in training dataset 
+    if row['gtdb-tk-species'] not in all_species:
+        index_species_not_exist.append(index)
+    # genus prediction with single child
+    elif row['genus'] in single_child_genus or row['gtdb-tk-genus'] in single_child_genus:
+        index_genus_single_child.append(index)
+
+
+print("index_species_not_exist is:", index_species_not_exist)
+print("index_genus_single_child is:", index_genus_single_child)
+        
+
