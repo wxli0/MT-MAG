86e11608c48e42e27a0803323bf348bddfa4d4e1
Wanxin Li <w328li@uwaterloo.ca>
2021-08-03 21:33:43 +0800

    debug

--- find_single_child.py
+++ find_single_child.py
@@ -1,9 +1,7 @@
 import os
 import pandas as pd 
 
-path1 = "./outputs-r202/MLDSP-prediction-full-path.csv"
-ranks1 = ['domain', 'phylum', 'class', 'order', 'family', 'genus']
-dir1 = '/mnt/sda/MLDSP-samples-r202/'
+
 def find_single_child(path, ranks, dir, suffix = ""):
     single_child_taxons = []
     df = pd.read_csv(path, index_col=0, header=0, dtype = str)
@@ -19,8 +17,16 @@ def find_single_child(path, ranks, dir, suffix = ""):
             single_child_taxons.append(taxon)
     return single_child_taxons
 
+path1 = "./outputs-r202/MLDSP-prediction-full-path.csv"
+ranks1 = ['domain', 'phylum', 'class', 'order', 'family', 'genus']
+dir1 = '/mnt/sda/MLDSP-samples-r202/'
+single_child_taxons1 = find_single_child(path1, ranks1, dir1)
+print("single_child taxons for GTDB are:", single_child_taxons1)
 
-single_child_taxons= find_single_child(path1, ranks1, dir1)
-print("single_child taxons for GTDB are:", single_child_taxons)
-
+path2 = "./outputs-r202/HGR-prediction-full-path.csv"
+ranks2 = ['phylum', 'class', 'order', 'family', 'genus']
+dir2 = '/mnt/sda/MLDSP-samples-r202/'
+suffix2 = "_split_pruned"
+single_child_taxons2 = find_single_child(path2, ranks2, dir2)
+print("single_child taxons for GTDB are:", single_child_taxons2)
 
