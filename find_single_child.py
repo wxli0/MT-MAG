43d88978b8ebc0c19798d921c40fb69a0985fe05
Wanxin Li <w328li@uwaterloo.ca>
2021-08-03 21:23:38 +0800

    added find_single_child.py

---
+++ find_single_child.py
@@ -0,0 +1,23 @@
+import os
+import pandas as pd 
+
+path1 = "~/BlindKameris-new/outputs-r202/MLDSP-prediction-full-path.csv"
+ranks1 = ['domain', 'phylum', 'class', 'order', 'family', 'genus']
+dir1 = '/mnt/sda/MLDSP-samples-r202/'
+def find_single_child(path, ranks, dir, suffix = ""):
+    single_child_taxons = []
+    df = pd.read_csv(path, index_col=0, header=0, dtype = str)
+    all_taxons = []
+    for rank in ranks:
+        all_taxons.append(list(df[rank]))
+    all_taxons = list(set(all_taxons))
+    for taxon in all_taxons:
+        if len(os.listdir(dir+taxon+suffix)) == 1:
+            single_child_taxons.append(taxon)
+    return single_child_taxons
+
+
+single_child_taxons= find_single_child(path1, ranks1, dir1)
+print("single_child taxons for GTDB are:", single_child_taxons)
+
+
