106147296f1d3f72503d6674dc38dc1c7b4120c0
Wanxin Li <w328li@uwaterloo.ca>
2021-08-03 21:30:47 +0800

    debug

--- find_single_child.py
+++ find_single_child.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd 
 
-path1 = "~/BlindKameris-new/outputs-r202/MLDSP-prediction-full-path.csv"
+path1 = "./outputs-r202/MLDSP-prediction-full-path.csv"
 ranks1 = ['domain', 'phylum', 'class', 'order', 'family', 'genus']
 dir1 = '/mnt/sda/MLDSP-samples-r202/'
 def find_single_child(path, ranks, dir, suffix = ""):
@@ -9,9 +9,12 @@ def find_single_child(path, ranks, dir, suffix = ""):
     df = pd.read_csv(path, index_col=0, header=0, dtype = str)
     all_taxons = []
     for rank in ranks:
-        all_taxons.append(list(df[rank]))
-    all_taxons = set(all_taxons).tolist()
+        all_taxons.extend(list(df[rank]))
+    all_taxons = list(set(all_taxons))
     for taxon in all_taxons:
+        taxon = str(taxon)
+        if 'reject' in taxon or taxon == 'nan':
+            continue
         if len(os.listdir(dir+taxon+suffix)) == 1:
             single_child_taxons.append(taxon)
     return single_child_taxons
