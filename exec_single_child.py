ed700b3fe19b4cd841728c70c2ade8a325695f4c
Wanxin Li <w328li@uwaterloo.ca>
2021-08-04 14:02:02 +0800

    added sleep in exec_single_child.py

--- exec_single_child.py
+++ exec_single_child.py
@@ -1,9 +1,8 @@
-import pandas as pd 
-import os 
-import sys
-import platform
-import subprocess
 from find_single_child import find_single_child
+import os 
+import pandas as pd 
+import subprocess
+import time
 
 # check single child taxons and exec single child classifications
 path1 = "./outputs-r202/MLDSP-prediction-full-path.csv"
@@ -19,31 +18,32 @@ suffix2 = "_split_pruned"
 single_child_taxons2 = find_single_child(path2, ranks2, dir2, suffix=suffix2)
 print("single_child taxons for HGR are:", single_child_taxons2)
 
+while True:
+    for taxon in single_child_taxons1:
+        running_proc = str(subprocess.check_output("ps aux|grep w328li|grep "+taxon, shell=True))
+        proc_all =  str(subprocess.check_output("screen -ls", shell=True))
+        if running_proc.count('\\n') <= 2 and proc_all.count('\\n') <= 40 \
+            and not os.path.exists('outputs-r202/test-c__Methanobacteria.xlsx'):
+                os.system("screen -dm bash -c "+"'''"+"cd ~/MLDSP; matlab -r "+'"run addme;stackedMain('+"'GTDB'"+", '"+taxon+"', 'rumen_mags/"+taxon+"');exit\"'''")
+                print("screen -dm bash -c "+"'''"+"cd ~/MLDSP; matlab -r "+'"run addme;stackedMain('+"'GTDB'"+", '"+taxon+"', 'rumen_mags/"+taxon+"');exit\"'''")
+        elif proc_all.count('\\n') > 40:
+            print('too many processes running')
+        elif running_proc.count('\\n') > 2:
+            print(taxon, "in GTDB running process")
+        else:
+            print(taxon, "GTDB completed")
 
-for taxon in single_child_taxons1:
-    running_proc = str(subprocess.check_output("ps aux|grep w328li|grep "+taxon, shell=True))
-    proc_all =  str(subprocess.check_output("screen -ls", shell=True))
-    if running_proc.count('\\n') <= 2 and proc_all.count('\\n') <= 40 \
-        and not os.path.exists('outputs-r202/test-c__Methanobacteria.xlsx'):
-            os.system("screen -dm bash -c "+"'''"+"cd ~/MLDSP; matlab -r "+'"run addme;stackedMain('+"'GTDB'"+", '"+taxon+"', 'rumen_mags/"+taxon+"');exit\"'''")
-            print("screen -dm bash -c "+"'''"+"cd ~/MLDSP; matlab -r "+'"run addme;stackedMain('+"'GTDB'"+", '"+taxon+"', 'rumen_mags/"+taxon+"');exit\"'''")
-    elif proc_all.count('\\n') > 40:
-        print('too many processes running')
-    elif running_proc.count('\\n') > 2:
-        print(taxon, "in GTDB running process")
-    else:
-        print(taxon, "GTDB completed")
-
-for taxon in single_child_taxons2:
-    running_proc = str(subprocess.check_output("ps aux|grep w328li|grep "+taxon, shell=True))
-    proc_all =  str(subprocess.check_output("screen -ls", shell=True))
-    if running_proc.count('\\n') <= 2 and proc_all.count('\\n') <= 40 \
-        and not os.path.exists('outputs-r202/test-c__Methanobacteria.xlsx'):
-            os.system("screen -dm bash -c "+"'''"+"cd ~/MLDSP; matlab -r "+'"run addme;stackedMain('+"'HGR'"+", '"+taxon+"', 'hgr_mags/"+taxon+"');exit\"'''")
-            print("screen -dm bash -c "+"'''"+"cd ~/MLDSP; matlab -r "+'"run addme;stackedMain('+"'HGR'"+", '"+taxon+"', 'hgr_mags/"+taxon+"');exit\"'''")
-    elif proc_all.count('\\n') > 40:
-        print('too many processes running')
-    elif running_proc.count('\\n') > 2:
-        print(taxon, "in HGR running process")
-    else:
-        print(taxon, "HGR completed")
+    for taxon in single_child_taxons2:
+        running_proc = str(subprocess.check_output("ps aux|grep w328li|grep "+taxon, shell=True))
+        proc_all =  str(subprocess.check_output("screen -ls", shell=True))
+        if running_proc.count('\\n') <= 2 and proc_all.count('\\n') <= 40 \
+            and not os.path.exists('outputs-r202/test-c__Methanobacteria.xlsx'):
+                os.system("screen -dm bash -c "+"'''"+"cd ~/MLDSP; matlab -r "+'"run addme;stackedMain('+"'HGR'"+", '"+taxon+"', 'hgr_mags/"+taxon+"');exit\"'''")
+                print("screen -dm bash -c "+"'''"+"cd ~/MLDSP; matlab -r "+'"run addme;stackedMain('+"'HGR'"+", '"+taxon+"', 'hgr_mags/"+taxon+"');exit\"'''")
+        elif proc_all.count('\\n') > 40:
+            print('too many processes running')
+        elif running_proc.count('\\n') > 2:
+            print(taxon, "in HGR running process")
+        else:
+            print(taxon, "HGR completed")
+    time.sleep(600) # sleep for 10 mins
