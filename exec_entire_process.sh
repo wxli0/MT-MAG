a61d2094a2ec59c66b7ef7497dde8b5f810a0e77
Wanxin Li <w328li@uwaterloo.ca>
2021-08-12 00:03:15 +0800

    removed exec GTDB from the script

--- exec_entire_process.sh
+++ exec_entire_process.sh
@@ -21,10 +21,10 @@ do
         python3 outputs-r202/group_pred.py order
         python3 outputs-r202/group_pred.py family
         python3 outputs-r202/group_pred.py genus
-        python3 outputs-HGR-r202/group_pred_HGR.py class
-        python3 outputs-HGR-r202/group_pred_HGR.py order
-        python3 outputs-HGR-r202/group_pred_HGR.py family
-        python3 outputs-HGR-r202/group_pred_HGR.py genus
+        # python3 outputs-HGR-r202/group_pred_HGR.py class
+        # python3 outputs-HGR-r202/group_pred_HGR.py order
+        # python3 outputs-HGR-r202/group_pred_HGR.py family
+        # python3 outputs-HGR-r202/group_pred_HGR.py genus
         echo "==== begin checking_missing_exec ====" 
         python3 checking_missing_exec.py True
     else
