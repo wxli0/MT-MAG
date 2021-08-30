3ca6c2583ba6d3d9a3234a49826771b4a5117d82
Wanxin Li <w328li@uwaterloo.ca>
2021-08-30 18:54:26 +0800

    updated exec_help.py

--- exec_helper.py
+++ exec_helper.py
@@ -123,11 +123,14 @@ def check_folders(data_type):
     """
     os.system("cd "+ config.MLDSP_path)
     if not os.path.isdir("outputs-"+data_type):
+        print("creating", os.path.join(config.MLDSP_path+"outputs-"+data_type))
         os.mkdir("outputs-"+data_type)
     os.system("cd "+config.MT_MAG_path)
     if not os.path.isdir("outputs-"+data_type):
         os.mkdir("outputs-"+data_type)
+        print("creating", os.path.join(config.MT_MAG_path+"outputs-"+data_type))
     if not os.path.isdir("rejection-threshold-"+data_type):
         os.mkdir("rejection-threshold-"+data_type)
+        print("creating", os.path.join(config.MT_MAG_path+"rejection-threshold-"+data_type))
     
 
