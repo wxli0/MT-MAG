3479cc342f8e7f9f60dc33a5e2c0f818c8dd47e3
Wanxin Li <w328li@uwaterloo.ca>
2021-08-30 19:06:33 +0800

    debug

--- exec_helper.py
+++ exec_helper.py
@@ -117,16 +117,18 @@ def check_folders(data_type, base_path, test_dir, pred_path, ranks):
     :type data_type:str
     """
     os.system("cd "+ config.MLDSP_path)
+    print("INFO: done cd "+ config.MLDSP_pat)
     if not os.path.isdir("outputs-"+data_type):
-        print("creating", os.path.join(config.MLDSP_path+"outputs-"+data_type))
+        print("creating", os.path.join(config.MLDSP_path, "outputs-"+data_type))
         os.mkdir("outputs-"+data_type)
     os.system("cd "+config.MT_MAG_path)
+    print("INFO: done cd "+config.MT_MAG_path)
     if not os.path.isdir("outputs-"+data_type):
         os.mkdir("outputs-"+data_type)
-        print("creating", os.path.join(config.MT_MAG_path+"outputs-"+data_type))
+        print("creating", os.path.join(config.MT_MAG_path, "outputs-"+data_type))
     if not os.path.isdir("rejection-threshold-"+data_type):
         os.mkdir("rejection-threshold-"+data_type)
-        print("creating", os.path.join(config.MT_MAG_path+"rejection-threshold-"+data_type))
+        print("creating", os.path.join(config.MT_MAG_path, "rejection-threshold-"+data_type))
     if not os.path.exists(pred_path):
         print("creating", pred_path)
         df = pd.DataFrame(columns=ranks)
