5cc5a8cbb6af60bc02812b678a8284f2b11b5942
Wanxin Li <w328li@uwaterloo.ca>
2021-09-01 23:21:34 +0800

    debug

--- paper/used_genome_size_anypath.py
+++ paper/used_genome_size_anypath.py
@@ -5,9 +5,10 @@ from Bio import SeqIO
 base_path = sys.argv[1]
 size = 0
 for file in os.listdir(base_path):
-    fasta_sequences = SeqIO.parse(open(os.path.join(base_path, file)),'fasta') 
-    for fasta in fasta_sequences:
-        _, sequence = fasta.id, str(fasta.seq)
-        size += len(sequence)
+    if file.endswith("_1.fa") or file.endswith('_2.fa'):
+        fasta_sequences = SeqIO.parse(open(os.path.join(base_path, file)),'fasta') 
+        for fasta in fasta_sequences:
+            _, sequence = fasta.id, str(fasta.seq)
+            size += len(sequence)
 
 print("base_path:", base_path, "size:", size)
