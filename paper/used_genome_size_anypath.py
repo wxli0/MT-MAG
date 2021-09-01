10ac5b4c53326b901067872b2ae141b4cbde092c
Wanxin Li <w328li@uwaterloo.ca>
2021-09-01 19:41:10 +0800

    debug

---
+++ paper/used_genome_size_anypath.py
@@ -0,0 +1,13 @@
+import os
+import sys
+from Bio import SeqIO
+
+base_path = sys.argv[1]
+size = 0
+for file in os.listdir(base_path):
+    fasta_sequences = SeqIO.parse(open(os.path.join(base_path, file)),'fasta') 
+    for fasta in fasta_sequences:
+        _, sequence = fasta.id, str(fasta.seq)
+        size += len(sequence)
+
+print("base_path:", base_path, "size:", size)
