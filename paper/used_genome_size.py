bed7afac359765e2a96f89bfd14a7a13d50314fc
Wanxin Li <w328li@uwaterloo.ca>
2021-09-05 18:01:58 +0800

    added contig_count, sample_count to used_genome_size

--- paper/used_genome_size.py
+++ paper/used_genome_size.py
@@ -1,5 +1,5 @@
 """
-Count the total genome size for task data_type
+Count the total genome genome_size for task data_type
 
 Command line arguments:
 
@@ -16,14 +16,18 @@ json_input = json.load(open(os.path.join('task_metadata', data_type+".json")))
 base_path = json_input['base_path']
 suffix = json_input['suffix']
 
-size = 0
+genome_size = 0
+contig_count = 0
+sample_count = 0
 for dir in os.listdir(base_path):
     if dir[1:3] == '__' and dir.endswith(suffix):
         for subdir in os.listdir(os.path.join(base_path, dir)):
             for file in os.listdir(os.path.join(base_path, dir, subdir)):
                 fasta_sequences = SeqIO.parse(open(os.path.join(base_path, dir, subdir, file)),'fasta') 
+                sample_count += 1
                 for fasta in fasta_sequences:
                     _, sequence = fasta.id, str(fasta.seq)
-                    size += len(sequence)
+                    genome_size += len(sequence)
+                    contig_count += 1
 
-print("total genome size for", data_type, "is:", size)
+print("base_path", base_path, "genome_size:", genome_size, "sample_count:", sample_count, "contig_count:", contig_count)
