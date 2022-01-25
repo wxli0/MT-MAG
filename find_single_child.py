f3b5c133c6354ebba7f94ca9320f6fefc9ea46e4
Wanxin Li <w328li@uwaterloo.ca>
2022-01-25 11:54:51 +0800

    updated find_single_child.py

--- find_single_child.py
+++ find_single_child.py
@@ -30,6 +30,19 @@ def find_single_child(path, ranks, dir, suffix = ""):
             single_child_taxons.append(taxon)
     return single_child_taxons
 
-find_single_child('~/MT-MAG/outputs-GTDB-r202-archive3/GTDB-r202-prediction-full-path.csv', \
+def update_single_child(single_child, output_dir):
+    for child in single_child:
+        child_file_path = os.path.join(output_dir, child+".xlsx")
+        df = pd.read_excel(child_file_path, index_col=0, header=0, sheet_name=child+"_pred-t-p")
+        max_list = df['max']
+        neg_filtered = filter(lambda score: score >= 70, max_list)
+        print(child, neg_filtered)
+
+
+
+GTDB_single_child = find_single_child('~/MT-MAG/outputs-GTDB-r202-archive3/GTDB-r202-prediction-full-path.csv', \
     ['domain', 'phylum', 'class', 'order', 'family', 'genus'], '/mnt/sda/MLDSP-samples-r202')
+# print("GTDB single child are:", GTDB_single_child)
+update_single_child(GTDB_single_child, '~/MT-MAG/outputs-GTDB-r202-archive3')
+
 
