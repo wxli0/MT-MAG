d95cdbea76f78a43084af7e94220759ad15f56bf
Wanxin Li <w328li@uwaterloo.ca>
2022-03-03 18:12:05 +0800

    debug

--- paper/size.py
+++ paper/size.py
@@ -4,11 +4,11 @@ import os
 import sys
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dir', default="", type=str, help='directories of the data')
+parser.add_argument('--data_path', default="", type=str, help='path of the data')
 parser.add_argument('--one_nested_folder', default=False, action="store_true", help='check only for files within a nested folder')
 parser.add_argument('--one_file', default=False, action="store_true", help='check for a specific file')
 args = parser.parse_args()
-dir = args.dir
+data_path = args.path
 file_num = 0
 contig_num = 0
 genome_size = 0
@@ -21,14 +21,21 @@ genome_size = 0
 #       - fasta
 #       - fasta
 if args.one_nested_folder:
-    for dir_nested in os.listdir(dir):
-        for fasta_file in os.listdir(os.path.join(dir, dir_nested)):
-            fasta_sequences = SeqIO.parse(open(os.path.join(dir, dir_nested, fasta_file)),'fasta') 
+    for dir_nested in os.listdir(data_path):
+        for fasta_file in os.listdir(os.path.join(data_path, dir_nested)):
+            fasta_sequences = SeqIO.parse(open(os.path.join(data_path, dir_nested, fasta_file)),'fasta') 
             for fasta in fasta_sequences:
                 _, sequence = fasta.id, str(fasta.seq)
                 genome_size += len(sequence)
                 contig_num += 1
             file_num += 1
+elif args.one_file:
+    fasta_sequences = SeqIO.parse(open(data_path,'fasta'))
+    for fasta in fasta_sequences:
+        _, sequence = fasta.id, str(fasta.seq)
+        genome_size += len(sequence)
+        contig_num += 1
+    file_num += 1
 
 print("file_num is:", file_num)
 print("contig_num is:", contig_num)
