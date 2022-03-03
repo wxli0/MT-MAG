fce4c69e2412f0004e863fff0d100df582af88c9
Wanxin Li <w328li@uwaterloo.ca>
2022-03-03 18:13:23 +0800

    debug

--- paper/size.py
+++ paper/size.py
@@ -8,7 +8,7 @@ parser.add_argument('--data_path', default="", type=str, help='path of the data'
 parser.add_argument('--one_nested_folder', default=False, action="store_true", help='check only for files within a nested folder')
 parser.add_argument('--one_file', default=False, action="store_true", help='check for a specific file')
 args = parser.parse_args()
-data_path = args.path
+data_path = args.data_path
 file_num = 0
 contig_num = 0
 genome_size = 0
@@ -20,6 +20,7 @@ genome_size = 0
 #   - p__Spirochaetota
 #       - fasta
 #       - fasta
+# e.g. python3 paper/size.py --dir /mnt/sda/DeepMicrobes-data/labeled_genome-r202/d__Bacteria --one_nested_folder
 if args.one_nested_folder:
     for dir_nested in os.listdir(data_path):
         for fasta_file in os.listdir(os.path.join(data_path, dir_nested)):
