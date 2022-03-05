ed4a3de5cf805c235b71db4f3fa817cc9aa83987
Wanxin Li <w328li@uwaterloo.ca>
2022-03-05 21:32:13 +0800

    increased tick size, removed confidence band and histograms in reliablity diagrams

--- reliability_diag_single.R
+++ reliability_diag_single.R
@@ -1,4 +1,5 @@
 library(reliabilitydiag)
+library(ggplot2)
 
 args = commandArgs(trailingOnly=TRUE)
 data_type <- args[1]
@@ -8,10 +9,12 @@ Y_file <- paste0('outputs-', data_type, '/', parent, '-rej-Y.txt')
 output_file <- paste0('outputs-', data_type, '/', parent, '-RD.png')
 X <- scan(X_file)
 Y <- scan(Y_file)
-res <- reliabilitydiag(x = X, y = Y)
+res <- reliabilitydiag(x = X, y = Y, region.level = NA)
+res_plot <- reliabilitydiag::autoplot(res, params_histogram = NA)
+res_final <- res_plot + theme(axis.text.x = element_text(size=14), axis.text.y = element_text(size=14))
 reliablity_score <- (summary(res))$miscalibration
 write(reliablity_score,file = paste0('outputs-', data_type, '/', parent, '-score.txt'),append=TRUE)
 
 png(output_file)
-print(res)
+print(res_final)
 dev.off()
