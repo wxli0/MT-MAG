568f5f4ac30c65fd7e5e718b21a721eea29d47ab
Wanxin Li <w328li@uwaterloo.ca>
2022-03-24 21:00:39 +0800

    added more cases to size.py

--- paper/size.py
+++ paper/size.py
@@ -5,6 +5,7 @@ import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--task',  default = 0, type=int, help='1 or 2')
+parser.add_argument('--type', default="", type=str, help='training or test')
 parser.add_argument('--tool', default="", type=str, help='DeepMicrobes or MT-MAG')
 args = parser.parse_args()
 task = args.task
@@ -13,9 +14,9 @@ file_num = 0
 contig_num = 0
 genome_size = 0
 
-def count(data_path, folder_path, suffix = "_trimmed.fa"):
+def count_train(data_path, folder_path, suffix = "_trimmed.fa"):
     """
-    Counts the genome size, contig number, file number in data_path within folder path
+    Counts the genome size, contig number, file number in data_path within folder path for test set
     Assuming the fasta file ends with suffix
     """
     genome_size = 0
@@ -31,48 +32,75 @@ def count(data_path, folder_path, suffix = "_trimmed.fa"):
             file_num += 1
     return genome_size, contig_num, file_num
 
-
-if args.task == 1 and args.tool  == "MT-MAG":
-    # command python3 paper/size.py --task=1 --tool=MT-MAG
-    data_path = "/mnt/sda/DeepMicrobes-data/labeled_genome-r202/d__Bacteria"
-    for dir_nested in os.listdir(data_path):
-        for fasta_file in os.listdir(os.path.join(data_path, dir_nested)):
-            fasta_sequences = SeqIO.parse(open(os.path.join(data_path, dir_nested, fasta_file)),'fasta') 
+def count_test(folder_path, suffix = "_trimmed.fa"):
+    """
+    Counts the genome size, contig number, file number in folder path for test set
+    Assuming the fasta file ends with suffix
+    """
+    genome_size = 0
+    contig_num = 0
+    file_num = 0
+    
+    for fasta_file in os.listdir(folder_path):
+        if fasta_file.endswith(suffix):
+            file_num += 1
+            fasta_sequences = SeqIO.parse(open(os.path.join(folder_path, fasta_file), 'fasta'))
             for fasta in fasta_sequences:
                 _, sequence = fasta.id, str(fasta.seq)
                 genome_size += len(sequence)
                 contig_num += 1
             file_num += 1
-elif args.task == 1 and args.tool == "DeepMicrobes":
-    # command: python3 paper/size.py --task=1 --tool=DeepMicrobes
-    data_path = "/mnt/sda/DeepMicrobes-data/labeled_genome_train_species_reads_author/author_train.fa"
-    folder_path = "/mnt/sda/DeepMicrobes-data/labeled_genome_train_species_reads_author"
-    genome_size, contig_num, file_num = count(data_path, folder_path)
-elif args.task == 2 and args.tool == "MT-MAG":
-    data_path = "/mnt/sda/MLDSP-samples-r202"
-    for dir_nested1 in os.listdir(data_path):
-        for dir_nested2 in os.listdir(data_path, dir_nested1):
-            for fasta_file in os.listdir(os.path.join(data_path, dir_nested1, dir_nested2)):
-                fasta_sequences = SeqIO.parse(open(os.path.join(data_path, dir_nested1, dir_nested2, fasta_file)),'fasta') 
+    return genome_size, contig_num, file_num
+
+if args.type == "training":
+    if args.task == 1 and args.tool  == "MT-MAG":
+        # command python3 paper/size.py --task=1 --tool=MT-MAG
+        data_path = "/mnt/sda/DeepMicrobes-data/labeled_genome-r202/d__Bacteria"
+        for dir_nested in os.listdir(data_path):
+            for fasta_file in os.listdir(os.path.join(data_path, dir_nested)):
+                fasta_sequences = SeqIO.parse(open(os.path.join(data_path, dir_nested, fasta_file)),'fasta') 
                 for fasta in fasta_sequences:
                     _, sequence = fasta.id, str(fasta.seq)
                     genome_size += len(sequence)
                     contig_num += 1
-                file_num += 1  
-elif args.task == 2 and args.tool == "DeepMicrobes":
-    # command: python3 paper/size.py --task=2 --tool=DeepMicrobes
-    data_path_11 = "/mnt/sda/MLDSP-samples-r202/GTDB_small_11_label_reads/Task2_small_11_all.fa"
-    folder_path_11 = "/mnt/sda/MLDSP-samples-r202/GTDB_small_11_label_reads/"
-    genome_size_11, contig_num_11, file_num_11 = count(data_path_11, folder_path_11)
-    data_path_1 = "/mnt/sda/MLDSP-samples-r202/GTDB_small_1_label_reads/Task2_small_1_all.fa"
-    folder_path_1 = "/mnt/sda/MLDSP-samples-r202/GTDB_small_1_label_reads/"
-    genome_size_1, contig_num_1, file_num_1 = count(data_path_11, folder_path_11)
-    data_path_2 = "/mnt/sda/MLDSP-samples-r202/GTDB_small_2_label_reads/Task2_small_2_all.fa"
-    folder_path_2 = "/mnt/sda/MLDSP-samples-r202/GTDB_small_2_label_reads/"
-    genome_size_2, contig_num_2, file_num_2 = count(data_path_11, folder_path_11)
-    genome_size = genome_size_11+genome_size_1+genome_size_2
-    contig_num = contig_num_11+contig_num_1+contig_num_2
-    file_num = file_num_11 # assume just use one file in the end
+                file_num += 1
+    elif args.task == 1 and args.tool == "DeepMicrobes":
+        # command: python3 paper/size.py --task=1 --tool=DeepMicrobes
+        data_path = "/mnt/sda/DeepMicrobes-data/labeled_genome_train_species_reads_author/author_train.fa"
+        folder_path = "/mnt/sda/DeepMicrobes-data/labeled_genome_train_species_reads_author"
+        genome_size, contig_num, file_num = count_train(data_path, folder_path)
+    elif args.task == 2 and args.tool == "MT-MAG":
+        data_path = "/mnt/sda/MLDSP-samples-r202"
+        for dir_nested1 in os.listdir(data_path):
+            for dir_nested2 in os.listdir(data_path, dir_nested1):
+                for fasta_file in os.listdir(os.path.join(data_path, dir_nested1, dir_nested2)):
+                    fasta_sequences = SeqIO.parse(open(os.path.join(data_path, dir_nested1, dir_nested2, fasta_file)),'fasta') 
+                    for fasta in fasta_sequences:
+                        _, sequence = fasta.id, str(fasta.seq)
+                        genome_size += len(sequence)
+                        contig_num += 1
+                    file_num += 1  
+    elif args.task == 2 and args.tool == "DeepMicrobes":
+        # command: python3 paper/size.py --task=2 --tool=DeepMicrobes
+        data_path_11 = "/mnt/sda/MLDSP-samples-r202/GTDB_small_11_label_reads/Task2_small_11_all.fa"
+        folder_path_11 = "/mnt/sda/MLDSP-samples-r202/GTDB_small_11_label_reads/"
+        genome_size_11, contig_num_11, file_num_11 = count_train(data_path_11, folder_path_11)
+        data_path_1 = "/mnt/sda/MLDSP-samples-r202/GTDB_small_1_label_reads/Task2_small_1_all.fa"
+        folder_path_1 = "/mnt/sda/MLDSP-samples-r202/GTDB_small_1_label_reads/"
+        genome_size_1, contig_num_1, file_num_1 = count_train(data_path_11, folder_path_11)
+        data_path_2 = "/mnt/sda/MLDSP-samples-r202/GTDB_small_2_label_reads/Task2_small_2_all.fa"
+        folder_path_2 = "/mnt/sda/MLDSP-samples-r202/GTDB_small_2_label_reads/"
+        genome_size_2, contig_num_2, file_num_2 = count_train(data_path_11, folder_path_11)
+        genome_size = genome_size_11+genome_size_1+genome_size_2
+        contig_num = contig_num_11+contig_num_1+contig_num_2
+        file_num = file_num_11 # assume just use one file in the end
+if args.type == "test":
+    if args.task == 1 and args.tool == "DeepMicrobes":
+        folder_path = "/mnt/sda/DeepMicrobes-data/mag_reads_150bp_1w_provided_split"
+        genome_size, contig_num, file_num = count_test(folder_path)
+    elif args.task == 2 and args.tool == "DeepMicrobes":
+        folder_path = "/mnt/sda/DeepMicrobes-data/rumen_mags_reads_Task2_small_all"
+        genome_size, contig_num, file_num = count_test(folder_path)
 
 print("file_num is:", file_num)
 print("contig_num is:", contig_num)
